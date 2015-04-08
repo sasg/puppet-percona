f68776c2ffa97925193c6a11306520de0a833f36
Timo Goebel <timo.goebel@dm.de>
2015-04-08 10:26:35 +0200

    add further spec tests

--- spec/classes/percona_spec.rb
+++ spec/classes/percona_spec.rb
@@ -12,6 +12,7 @@ describe 'percona', :type => :class do
         :concat_basedir         => '/dne',
         :path                   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
         :is_pe                  => false,
+        :memorysize_mb          => '1024',
       }
     end
     it { should compile.with_all_deps }
@@ -23,6 +24,33 @@ describe 'percona', :type => :class do
         { :db_galera => true }
       end
       it { should contain_package('nc') }
+      it { should contain_package('pv') }
+      it { should contain_package('socat') }
+
+      context 'without arbitor' do
+        let :params do
+          { :db_galera => true,
+            :is_arbitrator => false }
+        end
+        it { should contain_package('xinetd') }
+      end
+
+      context 'with low memory' do
+        let :facts do
+          {
+            :id                     => 'root',
+            :kernel                 => 'Linux',
+            :osfamily               => 'RedHat',
+            :operatingsystem        => 'RedHat',
+            :operatingsystemrelease => '6',
+            :concat_basedir         => '/dne',
+            :path                   => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+            :is_pe                  => false,
+            :memorysize_mb => '128'
+          }
+        end
+        it { should_not compile }
+      end
     end
   end
   context 'with unsupported osfamily' do
